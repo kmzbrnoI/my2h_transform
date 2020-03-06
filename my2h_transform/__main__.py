c84799a3d2c6edb48d153a520b9b0ab916383d1b
celestian <petr.celestian@gmail.com>
2020-03-06 19:16:03 +0100

    Datasets
    Source file is divided to the different sets of data. We need to work
    one by one because the structure is different.
    
    Also, we identified that L data are connection betweehn short name of
    railways and railway dataset.

--- my2h_transform/__main__.py
+++ my2h_transform/__main__.py
@@ -11,6 +11,7 @@ Options:
 
 
 from docopt import docopt
+import json
 
 
 def main():
@@ -18,16 +19,38 @@ def main():
     args = docopt(__doc__, version='0.0.1')  # pylint: disable=unused-variable
 
     if args['blocks']:
+
+        block_types = ['PNL', 'OR', 'OPM', 'OPD', 'L', 'W', 'T', 'H', 'B', 'C', 'A',
+                       'D', 'E', 'V', 'M', 'S', 'K', 'UV', 'Q', 'PST', 'EZ', 'N', 'R', 'P']
+
+        datasets = []
         with open(args['<myjop_blk_file>'], encoding='cp1250') as blk_file:
             lines = blk_file.readlines()
+            dataset = []
+            type_number = 0
+            for line in lines:
+                if line == '\n':
+                    datasets.append({'type': block_types[type_number], 'data': dataset})
+                    dataset = []
+                    type_number = type_number + 1
+                else:
+                    dataset.append(line.strip('\n'))
 
-        print_next = True
-        for line in lines:
-            if print_next:
-                print(line)
-                print_next = False
-            if line == '\n':
-                print_next = True
+        print('--- Datasets ---')
+        for dataset in datasets:
+            print(dataset['type'])
+
+        print('--- TR L blocks ---')
+        # block_types L = railway
+        # spojovaci tabulka mezi zkratkou trati a kolejemi?
+        # neobsahuje zadne ID, nutno nacit pozdeji
+        for dataset in datasets[block_types.index('L')]['data'][1:]:
+            data = dataset.split(';')
+            print(data)
+            railway = {
+                'name': data[3],
+            }
+            # print(json.dumps(railway, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
