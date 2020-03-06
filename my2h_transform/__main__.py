419e99d0c53c823f760dcf659a05c6d6ab50b3cd
celestian <petr.celestian@gmail.com>
2020-03-06 12:03:01 +0100

    List of sections in original file
    Original .blk file contains different sets of data. For transformation
    we want to parse each of them separatelly.

--- my2h_transform/__main__.py
+++ my2h_transform/__main__.py
@@ -1,6 +1,6 @@
-"""door
+"""My2h JOP Transform Utility
 Usage:
-  my2h_transform.py
+  my2h_transform.py blocks <myjop_blk_file>
   my2h_transform.py (-h | --help)
   my2h_transform.py --version
 
@@ -17,7 +17,17 @@ def main():
     '''Entry point'''
     args = docopt(__doc__, version='0.0.1')  # pylint: disable=unused-variable
 
-    print('Hello World')
+    if args['blocks']:
+        with open(args['<myjop_blk_file>'], encoding='cp1250') as blk_file:
+            lines = blk_file.readlines()
+
+        print_next = True
+        for line in lines:
+            if print_next:
+                print(line)
+                print_next = False
+            if line == '\n':
+                print_next = True
 
 
 if __name__ == '__main__':
