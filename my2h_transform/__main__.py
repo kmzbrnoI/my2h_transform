92788c86a9da3c10410d3aaa30085fafec49b024
celestian <petr.celestian@gmail.com>
2020-03-06 20:26:45 +0100

    Add BL T blocks
    We created table fot BL T blocks. It is entitiy with those attributes:
    * id
    * label
    * group
    * velocity
    * and some others

--- my2h_transform/__main__.py
+++ my2h_transform/__main__.py
@@ -13,10 +13,14 @@ Options:
 from docopt import docopt
 import json
 
+from storage import Storage
+
 
 def main():
     '''Entry point'''
     args = docopt(__doc__, version='0.0.1')  # pylint: disable=unused-variable
+
+    storage = Storage()
 
     if args['blocks']:
 
@@ -51,6 +55,48 @@ def main():
                 'name': data[3],
             }
             # print(json.dumps(railway, sort_keys=True, indent=4))
+
+        print('--- BL T blocks ---')
+        # trate?
+        blts = []
+        for dataset in datasets[block_types.index('T')]['data'][1:]:
+            data = dataset.split(';')
+            item = {
+                'id': data[3],
+                'groupt': data[4],
+                'label': data[5],
+                'label_text_part': data[6],
+                'gate_type': data[7],
+                'velocity': data[12],
+                'det1': data[15],
+                'det2': data[16],
+                'det3': data[17],
+                'det4': data[18],
+                'boost': data[19],
+                'zdroj': data[20],
+                'L': data[21],
+                'LL': data[22],
+                'S': data[23],
+                'SS': data[24],
+                'nvLtyp': data[25],
+                'hw1': data[26],
+                'lout1': data[27],
+                'lout2': data[28],
+                'lout3': data[29],
+                'lout4': data[30],
+                'lout5': data[31],
+                'nvStyp': data[32],
+                'hw2': data[33],
+                'sout1': data[34],
+                'sout2': data[35],
+                'sout3': data[36],
+                'sout4': data[37],
+                'sout5': data[38],
+            }
+            blts.append(item)
+            print(json.dumps(item, sort_keys=True, indent=4))
+
+        storage.save_blts(blts)
 
 
 if __name__ == '__main__':
---
+++ my2h_transform/storage.py
@@ -0,0 +1,72 @@
+import sqlite3
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+class Storage:
+
+    def __init__(self):
+
+        self._conn = sqlite3.connect('./blk.sql')
+        self._conn.row_factory = dict_factory
+        self._cursor = self._conn.cursor()
+
+        self._cursor.execute('''
+            CREATE TABLE IF NOT EXISTS blt (
+                id INTEGER PRIMARY KEY,
+                groupt TEXT,
+                label TEXT,
+                label_text_part TEXT,
+                gate_type TEXT,
+                velocity INTEGER,
+                det1 TEXT,
+                det2 TEXT,
+                det3 TEXT,
+                det4 TEXT,
+                boost INTEGER,
+                zdroj TEXT,
+                L TEXT,
+                LL TEXT,
+                S TEXT,
+                SS TEXT,
+                nvLtyp TEXT,
+                hw1 TEXT,
+                lout1 TEXT,
+                lout2 TEXT,
+                lout3 TEXT,
+                lout4 TEXT,
+                lout5 TEXT,
+                nvStyp TEXT,
+                hw2 TEXT,
+                sout1 TEXT,
+                sout2 TEXT,
+                sout3 TEXT,
+                sout4 TEXT,
+                sout5 TEXT
+            );
+        ''')
+        self._conn.commit()
+
+    def __del__(self):
+        self._conn.close()
+
+    def save_blts(self, blts):
+
+        if len(blts) == 0:
+            return
+
+        columns = ', '.join(blts[0].keys())
+        placeholders = ', '.join('?' * len(blts[0]))
+        sql = 'REPLACE INTO blt ({}) VALUES ({});'.format(columns, placeholders)
+
+        values = []
+        for item in blts:
+            values.append(list(item.values()))
+
+        self._cursor.executemany(sql, values)
+        self._conn.commit()
