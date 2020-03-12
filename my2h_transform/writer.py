88f02f57a1bac75656d35a5f83a1b34a9e1cc58b
Jan Horacek <me@apophis.cz>
2020-03-12 20:45:43 +0100

    Export railway signals too.

--- my2h_transform/writer.py
+++ my2h_transform/writer.py
@@ -103,52 +103,71 @@ def write_section(session):
     return blocks
 
 
+def signal_data(signal, name):
+    data = {
+        'nazev': name,
+        'typ': 3,
+    }
+
+    rcs_count = 0
+    for det in (signal.out1, signal.out2, signal.out3, signal.out4):
+        if det != '0:0':
+            rcs_count = rcs_count + 1
+
+    RCSb0 = int(signal.out1.split(':', 1)[0])
+    RCSp0 = int(signal.out1.split(':', 1)[1])
+    RCSb1 = int(signal.out2.split(':', 1)[0])
+    RCSp1 = int(signal.out2.split(':', 1)[1])
+    RCSb2 = int(signal.out3.split(':', 1)[0])
+    RCSp2 = int(signal.out3.split(':', 1)[1])
+    RCSb3 = int(signal.out4.split(':', 1)[0])
+    RCSp3 = int(signal.out4.split(':', 1)[1])
+
+    if rcs_count != 0:
+        data['RCScnt'] = rcs_count
+
+    if RCSb0 != 0:
+        data['RCSb0'] = RCSb0
+        data['RCSp0'] = RCSp0
+
+    if RCSb1 != 0:
+        data['RCSb1'] = RCSb1
+        data['RCSp1'] = RCSp1
+
+    if RCSb2 != 0:
+        data['RCSb2'] = RCSb2
+        data['RCSp2'] = RCSp2
+
+    if RCSb3 != 0:
+        data['RCSb3'] = RCSb3
+        data['RCSp3'] = RCSp3
+
+    if rcs_count:
+        data['OutType'] = 0
+
+    return data
+
+
 def write_signal(session):
 
     blocks = []
+
+    # area signals
     for signal, area in session.query(
             Signal, Control_Area).filter(Signal.control_area == Control_Area.id).order_by(Signal.id).all():
+        name = area.shortname.split(' ', 1)[0].capitalize() + ' ' + signal.name
+        data = signal_data(signal, name)
 
-        data = {
-            'nazev': '{} {}'.format(area.shortname.split(' ', 1)[0].capitalize(), signal.name),
-            'typ': 3,
-        }
+        blocks.append({
+            'id': signal.id,
+            'data': data,
+        })
 
-        rcs_count = 0
-        for det in (signal.out1, signal.out2, signal.out3, signal.out4):
-            if det != '0:0':
-                rcs_count = rcs_count + 1
-
-        RCSb0 = int(signal.out1.split(':', 1)[0])
-        RCSp0 = int(signal.out1.split(':', 1)[1])
-        RCSb1 = int(signal.out2.split(':', 1)[0])
-        RCSp1 = int(signal.out2.split(':', 1)[1])
-        RCSb2 = int(signal.out3.split(':', 1)[0])
-        RCSp2 = int(signal.out3.split(':', 1)[1])
-        RCSb3 = int(signal.out4.split(':', 1)[0])
-        RCSp3 = int(signal.out4.split(':', 1)[1])
-
-        if rcs_count != 0:
-            data['RCScnt'] = rcs_count
-
-        if RCSb0 != 0:
-            data['RCSb0'] = RCSb0
-            data['RCSp0'] = RCSp0
-
-        if RCSb1 != 0:
-            data['RCSb1'] = RCSb1
-            data['RCSp1'] = RCSp1
-
-        if RCSb2 != 0:
-            data['RCSb2'] = RCSb2
-            data['RCSp2'] = RCSp2
-
-        if RCSb3 != 0:
-            data['RCSb3'] = RCSb3
-            data['RCSp3'] = RCSp3
-
-        if rcs_count:
-            data['OutType'] = 0
+    # railway signals
+    for signal, railway in session.query(
+            Signal, Railway).filter(Signal.control_area == None, Signal.trat1 == Railway.id).order_by(Signal.id).all():
+        name = railway.shortname+ ' ' + signal.name
+        data = signal_data(signal, name)
 
         blocks.append({
             'id': signal.id,
